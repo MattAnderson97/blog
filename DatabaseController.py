1bf5fa169d62bd65fcb195f72e13d42c4dd6f58a
mattanderson97 <mattanderson97@users.noreply.github.com>
2021-02-03 16:05:52 +0000

    add query for creating tables in DB controller, add query for enabling foreign keys, create users and posts tables

--- DatabaseController.py
+++ DatabaseController.py
@@ -6,24 +6,42 @@ class DatabaseController:
         self.db_name = db_name
         self.conn = None
         self.conn_open = False
+        self.open_conn()
+        # enable foreign keys
+        cur = self.conn.cursor()
+        cur.execute('PRAGMA foreign_keys = ON;')
 
     def open_conn(self):
         if !self.conn_open:
+            # create database connection
             self.conn = sqlite3.connect(self.db_name)
             self.conn_open = True
     
     def close_conn(self):
         if self.conn_open and self.conn is not None:
+            # close database connection
             self.conn.close()
             self.conn_open = False
 
     def create_table(self, name, *args):
         rows = args.join(", ")
-        query = f"create table {name} ({args})"
-
+        query = f"create table if not exists {name} ({args});"
+        # make sure a database connection is active
+        if (!self.conn_open):
+            self.open_conn()
+        # get database cursor
+        cur = self.conn.cursor()
+        cur.execute(query)
+        # save changes to database
+        self.conn.commit()
 
     def execute_query(self, query, data):
+        # make sure a database connection is active
+        if (!self.conn_open):
+            self.open_conn()
+        # get database cursor
         cur = self.conn.cursor()
         cur.execute(query, data)
+        # save changes to database
         self.conn.commit()
         return cur.fetchall()
--- app.py
+++ app.py
@@ -1,9 +1,34 @@
 from flask import Flask
 
+from DatabaseController import DatabaseController
 from routes import routes
 
 app = Flask(__name__)
 app.register_blueprint(routes)
+
+db = DatabaseController("data")
+
+
+def create_tables():
+    db.create_table(
+        'users', 
+        'UUID INTEGER PRIMARY KEY NOT NULL', 
+        'name TEXT NOT NULL', 
+        'password_hash TEXT NOT NULL', 
+        'password_salt TEXT NOT NULL'
+    )
+    db.create_table(
+        'posts', 
+        'post_id INTEGER PRIMARY KEY NOT NULL AUTOINCREMENT', 
+        'post_title TEXT NOT NULL', 
+        'post_description TEXT', 
+        'post_thumbnail BLOB', 
+        'post_categories TEXT', 
+        'post_date TEXT NOT NULL', 
+        'post_content TEXT NOT NULL',
+        'author_id INTEGER NOT NULL', 
+        'FOREIGN KEY (author_id) REFERENCES Users(UUID)'
+    )
 
 
 if __name__ == "__main__":
